0221f624f6295294c32dbc9edb186c0867b6fc9a
LisaWedin_Ductus <lisa.wedin@ductus.se>
2026-07-07 19:18:32 +0200

    fix

--- src/main/java/se/fk/github/portaladminbff/model/OulSorteringsordningEntry.java
+++ src/main/java/se/fk/github/portaladminbff/model/OulSorteringsordningEntry.java
@@ -1,10 +1,12 @@
 package se.fk.github.portaladminbff.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OulSorteringsordningEntry
 {
    public List<Map<String, Object>> constraints;
